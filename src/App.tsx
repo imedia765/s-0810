1922bdf34c95163b376b28ca261bd55efd1c2eb8
imedia765 <137381353+imedia765@users.noreply.github.com>
2025-01-11 03:30:31 +0000

    Refactor project structure and update title; remove unused components and update README links

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import CodeGenerator from "./pages/CodeGenerator";
-import AIResearcher from "./pages/AIResearcher";
+import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
 
@@ -16,9 +14,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/code-generator" element={<CodeGenerator />} />
-          <Route path="/researcher" element={<AIResearcher />} />
+          <Route path="/" element={<Index />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
